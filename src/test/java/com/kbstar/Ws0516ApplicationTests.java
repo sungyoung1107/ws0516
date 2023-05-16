ea61933e2feab6d7a91ce460d4cd25819224ab7f
sungyoung1107 <dong09161107@gmail.com>
2023-05-16 17:53:22 +0900

    Initial commit

---
+++ src/test/java/com/kbstar/Ws0516ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbstar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ws0516ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
